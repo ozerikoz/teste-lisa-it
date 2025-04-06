cb67fc3ab68514c69b0b1671d2e159c07ffbf741
ozerikoz <erik.paschoalatto1@gmail.com>
2025-04-06 19:46:47 -0300

    feat: add auth repository interface

---
+++ lib/data/repositories/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'package:teste_lisa_it/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+  /// Performs user login with the given [email] and [password].
+  Future<User> login({
+    required String email,
+    required String password,
+  });
+
+  /// Logs out the current user.
+  Future<void> logout();
+
+  /// Checks if the user is logged in.
+  Future<bool> isLoggedIn();
+}
