16ac6ad9db196076192ce93fef936a8c77d4c83a
ozerikoz <erik.paschoalatto1@gmail.com>
2025-04-10 18:52:40 -0300

    feat: add TesteLisaAppBar

---
+++ lib/presentation/core/widgets/app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:teste_lisa_it/presentation/auth/logout/widgets/logout_button.dart';
+
+/// A custom AppBar widget with [LogoutButton].
+class TesteLisaAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const TesteLisaAppBar({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      actions: [
+        LogoutButton(),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
