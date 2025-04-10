482e5b17b993302a367635617b8b4332df597857
ozerikoz <erik.paschoalatto1@gmail.com>
2025-04-10 16:46:10 -0300

    feat: add Routes for app navigation

---
+++ lib/core/router/routes.dart
@@ -0,0 +1,6 @@
+abstract final class Routes {
+  static const home = '/';
+  static const login = '/login';
+  static const postDetails = '/post/details';
+  static const postUserProfile = '/post/user/profile';
+}
