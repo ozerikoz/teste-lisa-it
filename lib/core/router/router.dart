d3c7318a3cdaac4fb851fad0cc33b649322b535f
ozerikoz <erik.paschoalatto1@gmail.com>
2025-04-11 10:00:49 -0300

    feat: add routes for PostDetailsPage and PostUserProfilePage

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -6,7 +6,11 @@ import 'package:teste_lisa_it/core/router/routes.dart';
 import 'package:teste_lisa_it/presentation/auth/login/bloc/login_bloc.dart';
 import 'package:teste_lisa_it/presentation/auth/login/pages/login_page.dart';
 import 'package:teste_lisa_it/presentation/home/bloc/posts_bloc.dart';
+import 'package:teste_lisa_it/presentation/home/models/post_ui_model.dart';
+import 'package:teste_lisa_it/presentation/home/models/post_user_profile_ui_model.dart';
 import 'package:teste_lisa_it/presentation/home/pages/home_page.dart';
+import 'package:teste_lisa_it/presentation/home/pages/post/post_details_page.dart';
+import 'package:teste_lisa_it/presentation/home/pages/post/post_user_profile_page.dart';
 
 /// Router configuration for the app.
 /// Listens to changes in [AuthBloc] to redirect the user
@@ -38,6 +42,27 @@ GoRouter router() => GoRouter(
               ),
             child: HomePage(),
           ),
+        ),
+        GoRoute(
+          path: Routes.postDetails,
+          builder: (context, state) {
+            PostUIModel post = state.extra as PostUIModel;
+
+            return PostDetailsPage(
+              post: post,
+            );
+          },
+        ),
+        GoRoute(
+          path: Routes.postUserProfile,
+          builder: (context, state) {
+            PostUserProfileUIModel postUser =
+                state.extra as PostUserProfileUIModel;
+
+            return PostUserProfilePage(
+              postUser: postUser,
+            );
+          },
         ),
       ],
     );
