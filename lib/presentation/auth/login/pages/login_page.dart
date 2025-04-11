f3763b2c4895a1dbe365ceff099d7f9c24d44d32
ozerikoz <erik.paschoalatto1@gmail.com>
2025-04-11 10:14:16 -0300

    refactor: remove unused import from login_page.dart

--- lib/presentation/auth/login/pages/login_page.dart
+++ lib/presentation/auth/login/pages/login_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:teste_lisa_it/presentation/auth/login/bloc/login_bloc.dart';
