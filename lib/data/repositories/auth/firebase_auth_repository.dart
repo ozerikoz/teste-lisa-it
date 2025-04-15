6c67f097ebbcb4bab6c4b5d1395c9b0a1d99a190
ozerikoz <erik.paschoalatto1@gmail.com>
2025-04-15 18:36:30 -0300

    refactor: add FirebaseAuthRepository exception handling

--- lib/data/repositories/auth/firebase_auth_repository.dart
+++ lib/data/repositories/auth/firebase_auth_repository.dart
@@ -1,3 +1,4 @@
+import 'package:teste_lisa_it/core/exceptions/exceptions.dart';
 import 'package:teste_lisa_it/data/repositories/auth/auth_repository.dart';
 import 'package:teste_lisa_it/data/services/auth/firebase_auth_service.dart';
 import 'package:teste_lisa_it/domain/entities/user/user_entity.dart';
@@ -42,8 +43,10 @@ class FirebaseAuthRepository extends AuthRepository {
       _user = user;
 
       return user;
-    } catch (e) {
-      throw UnimplementedError();
+    } on AppException {
+      rethrow;
+    } catch (e, s) {
+      throw UnknownException("Error during login", s);
     }
   }
 
@@ -51,8 +54,10 @@ class FirebaseAuthRepository extends AuthRepository {
   Future<void> logout() {
     try {
       return _firebaseAuthService.logout();
-    } catch (e) {
-      throw UnimplementedError();
+    } on AppException {
+      rethrow;
+    } catch (e, s) {
+      throw UnknownException("Error during logout", s);
     }
   }
 
