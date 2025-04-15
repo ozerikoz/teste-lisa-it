b784575a2e9631420102ecdab135e068ca575c41
ozerikoz <erik.paschoalatto1@gmail.com>
2025-04-15 18:33:39 -0300

    feat: add FirebaseAuthService

--- lib/data/services/auth/firebase_auth_service.dart
+++ lib/data/services/auth/firebase_auth_service.dart
@@ -1,12 +1,17 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import "package:firebase_auth/firebase_auth.dart";
+import "package:teste_lisa_it/data/exceptions/auth_exceptions.dart";
+import "package:teste_lisa_it/core/exceptions/exceptions.dart";
 
+/// This class provides authentication services using Firebase Authentication.
 class FirebaseAuthService {
+  /// The [FirebaseAuth] instance used for authentication.
   final FirebaseAuth _firebaseAuth;
 
-  const FirebaseAuthService({
-    required FirebaseAuth firebaseAuth,
-  }) : _firebaseAuth = firebaseAuth;
+  FirebaseAuthService({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
 
+  /// Perform [FirebaseAuth] login with email and password.
   Future<UserCredential> login({
     required String email,
     required String password,
@@ -19,21 +24,40 @@ class FirebaseAuthService {
       );
 
       return user;
-    } on FirebaseAuthException {
-      rethrow;
-    } catch (e) {
-      throw Exception("login failed, error: $e");
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "invalid-email":
+        case "wrong-password":
+        case "user-not-found":
+        case "invalid-credential":
+          throw InvalidCredentialsException(e.code);
+        case 'network-request-failed':
+          throw NetworkException();
+        default:
+          throw AuthException("Authentication error: ${e.message}");
+      }
+    } catch (e, s) {
+      throw UnknownException("Unexpected exception: $e", s);
     }
   }
 
+  /// Perform [FirebaseAuth] sign out
   Future<void> logout() async {
     try {
       await _firebaseAuth.signOut();
-    } catch (e) {
-      throw Exception("logout failed, error: $e");
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'network-request-failed':
+          throw NetworkException();
+        default:
+          throw AuthException('Logout error: ${e.message}');
+      }
+    } catch (e, s) {
+      throw UnknownException("Unexpected exception: $e", s);
     }
   }
 
+  /// Get the current user from [FirebaseAuth]
   User? getCurrentUser() {
     return _firebaseAuth.currentUser;
   }
